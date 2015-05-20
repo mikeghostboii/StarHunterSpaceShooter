aa58de247e6cb4e053ef8324d3feb64315a2ffb5
Michael Wasihun <mikeghostboii@users.noreply.github.com>
2015-05-20 18:45:50 -0400

    Update BattleShip.java

--- src/BattleShip.java
+++ src/BattleShip.java
@@ -7,7 +7,7 @@ public class BattleShip extends Ship {
 	private int width = 25;
 	private int height = 25;
 
-	Rectangle box, body, tip;
+	Rectangle box, body;
 
 	public BattleShip() {
 		super();
@@ -18,13 +18,11 @@ public class BattleShip extends Ship {
 	}
 
 	public void draw(Graphics2D g2) {
-		int xCor = x;
-		int yCor = y;
-		box = new Rectangle(xCor, yCor, width, height);
+		box = new Rectangle((int) x, (int) y, width, height);
 		g2.draw(box);
 		g2.fill(box);
 		g2.setColor(Color.YELLOW);
-		body = new Rectangle(x, y, width - 15, height - 15);
+		body = new Rectangle((int) x, (int) y, width - 15, height - 15);
 		g2.fill(body);
 	}
 
