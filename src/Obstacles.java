19e6222326f5175a97ec7cb3efed2adbc35d459a
Michael Wasihun <mikeghostboii@users.noreply.github.com>
2015-05-20 18:48:25 -0400

    Update Obstacles.java

--- src/Obstacles.java
+++ src/Obstacles.java
@@ -4,7 +4,6 @@ import java.awt.geom.Ellipse2D;
 
 public class Obstacles extends Ship {
 
-	int x, y;
 	private int width = 5;
 	private int height = 5;
 	
@@ -14,18 +13,16 @@ public class Obstacles extends Ship {
 		super();
 	}
 	public Obstacles(int x, int y){
-		this.x = x;
-		this.y = y;
+		super(x, y);
 	}
 	
 	public void draw(Graphics2D g2){
-		int xCor = x;
-		int yCor = y;			
-    	circle = new Ellipse2D.Double(xCor, yCor, width, height);
+    		circle = new Ellipse2D.Double(x, y, width, height);
 		g2.setColor(Color.DARK_GRAY);
 		g2.fill(circle);
 		g2.draw(circle);
 	}
 	
 }
+ 
  
